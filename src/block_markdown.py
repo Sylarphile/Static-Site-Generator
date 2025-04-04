185550ac62fd5ebbb26a36623eedd31b5cfa3686
ryan <sylarphile@gmail.com>
2025-04-03 23:53:11 -0700

    added BlockType Class

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,29 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if re.match(r'^#{1,6} ', block):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.ULIST
+    if all(re.match(fr"^{i + 1}\. ", line) for i, line in enumerate(lines)):
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+    
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     new_blocks = []
--- src/test_block.py
+++ src/test_block.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -46,6 +46,56 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_type_ordered(self):
+        md = """
+1. test
+2. test2
+3. test3
+""".strip()
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.OLIST)
+
+    def test_block_to_block_type_code(self):
+        md = """```print('hello world')```"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_heading(self):
+        md = """### hello world"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_unordered(self):
+        md = """- test
+- test2
+- test3"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.ULIST)
+
+    def test_block_to_block_type_code(self):
+        md = """>Hello
+>World"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_blank(self):
+        md = """"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
